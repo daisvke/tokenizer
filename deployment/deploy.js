3beba9972bdf3ce8e12dd7da47891332431ae571
dtanigaw <dtanigaw@student.42.fr>
2025-06-09 17:10:45 +0200

    Add deploy+interact

--- deployment/deploy.js
+++ deployment/deploy.js
@@ -1,11 +1,13 @@
 const INITIAL_SUPPLY = process.env.INITIAL_SUPPLY
 
 async function main() {
-	const initialSupply = ethers.utils.parseUnits(INITIAL_SUPPLY, 18); // 1000 D42 tokens
+	// Set an initial token supply of 1000 D42 tokens
+	const initialSupply = ethers.utils.parseUnits(INITIAL_SUPPLY, 18);
 	// Get a contract factory object to allow deployment
 	const D42 = await ethers.getContractFactory("d42");
 	// Start deployment, returning a promise that resolves to a contract object
 	const d42 = await D42.deploy(initialSupply);
+	// Make sure to wait until it is deployed
 	await d42.deployed();
 
 	console.log("Contract deployed to:", d42.address);
--- deployment/interact.js
+++ deployment/interact.js
@@ -6,11 +6,17 @@ const contract = require("../code/artifacts/contracts/d42.sol/d42.json");
 const provider = new ethers.providers.InfuraProvider("sepolia", API_KEY);
 const signer = new ethers.Wallet(PRIVATE_KEY, provider);
 
+// Contract instance
 const token = new ethers.Contract(CONTRACT_ADDRESS, contract.abi, signer);
 
+// A function to check the balance of the contract
 async function checkBalance() {
 	const balance = await token.balanceOf(signer.address);
 	console.log("Your balance:", ethers.utils.formatUnits(balance, 18), "D42");
 }
 
-checkBalance();
+async function main() {
+	checkBalance();
+}
+
+main().catch(console.error);
