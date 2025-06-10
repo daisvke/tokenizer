05c9e061ae93f8693a6b280f895ada977315143a
daisvke <daisuketanigawa@live.fr>
2025-06-10 16:30:21 +0200

    improve print style

--- deployment/interact.js
+++ deployment/interact.js
@@ -1,22 +1,75 @@
-const API_KEY = process.env.API_KEY
-const PRIVATE_KEY = process.env.PRIVATE_KEY
-const CONTRACT_ADDRESS = process.env.DEPLOYED_CONTRACT_ADDRESS
-const contract = require("../code/artifacts/contracts/d42.sol/d42.json");
+// Get the provider that gives access to the testnet blockchain
+const provider = new ethers.providers.JsonRpcProvider(process.env.API_URL);
+// The address we want to send tokens to
+const RECEIVER_ADDRESS = process.env.WALLET_ADDRESS_OTHER;
+// Get the initial owner account's supply amount
+const INITIAL_SUPPLY = process.env.INITIAL_SUPPLY
 
-const provider = new ethers.providers.InfuraProvider("sepolia", API_KEY);
-const signer = new ethers.Wallet(PRIVATE_KEY, provider);
+// Get the wallets
+const owner = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
+const other = new ethers.Wallet(process.env.PRIVATE_KEY_OTHER, provider);
 
-// Contract instance
-const token = new ethers.Contract(CONTRACT_ADDRESS, contract.abi, signer);
+console.log("├── Owner's wallet address:", owner.address);
+console.log("├── Other's wallet address:", other.address);
 
-// A function to check the balance of the contract
-async function checkBalance() {
-	const balance = await token.balanceOf(signer.address);
-	console.log("Your balance:", ethers.utils.formatUnits(balance, 18), "D42");
+async function getBalances(d42) {
+	// Get the balance of the two accounts
+	const balance = await d42.balanceOf(owner.address);
+	const balanceOther = await d42.balanceOf(RECEIVER_ADDRESS);
+	// Get the number of decimals from the token contract
+	const decimals = await d42.decimals();
+	// Convert the wei values to tokens and print
+	console.log("=======================");
+	console.log("Owner balance:", ethers.utils.formatUnits(balance, decimals));
+	console.log("Other balance:", ethers.utils.formatUnits(balanceOther, decimals));
+	console.log("=======================");
 }
 
 async function main() {
-	checkBalance();
+	// Get the contract factory object of our smart contract
+	const D42 = await ethers.getContractFactory("d42", owner);
+	// Deploy the contract on the testnet
+	const d42 = await D42.deploy(INITIAL_SUPPLY);
+	// Make sure to wait until it is deployed
+	await d42.deployed();
+
+	const name = await d42.name();
+	const symbol = await d42.symbol();
+	console.log(`└── Token name: ${name} | Symbol: ${symbol}`);
+
+	console.log(`\n✅ Contract deployed to address: ${d42.address}\n`);
+
+	try {
+		// Connection test with Other (not supposed to succeed)
+		// const d42FromOther = d42.connect(other);
+		// await d42FromOther.mint(other.address, ethers.utils.parseEther("100"));
+
+		// Check original balance of owner
+		await getBalances(d42);
+
+		// Transfer tokens to 'Other'
+		const amount = "10"; // sending 10 tokens
+
+		console.log(`Sending ${amount} tokens to address: ${RECEIVER_ADDRESS}...`);
+
+		// Convert the transfered amount in wei
+		const tx = await d42.transfer(RECEIVER_ADDRESS, ethers.utils.parseEther(amount));
+		await tx.wait();
+
+  		console.log("\n✅ Transfer successful!\n");
+
+		// Check updated balance of owner
+		await getBalances(d42);
+
+	} catch (err) {
+		console.error("Minting failed from non-owner account, as expected:");
+		console.error(err.message);
+	}
 }
 
-main().catch(console.error);
+main()
+	.then(() => process.exit(0))
+	.catch((err) => {
+		console.error(err);
+		process.exit(1);
+	});
--- deployment/tests.js
+++ deployment/tests.js
@@ -1,28 +1,43 @@
+// Get the provider that gives access to the testnet blockchain
 const provider = new ethers.providers.JsonRpcProvider(process.env.API_URL);
-const RECEIVER_ADDRESS = process.env.WALLET_ADDRESS_OTHER; // the address you want to send tokens to
+// The address we want to send tokens to
+const RECEIVER_ADDRESS = process.env.WALLET_ADDRESS_OTHER;
+// Get the initial owner account's supply amount
 const INITIAL_SUPPLY = process.env.INITIAL_SUPPLY
 
+// Get the wallets
 const owner = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
 const other = new ethers.Wallet(process.env.PRIVATE_KEY_OTHER, provider);
 
-console.log("Owner:", owner.address);
-console.log("Other:", other.address);
+console.log("Owner's wallet address:", owner.address);
+console.log("Other's wallet address:", other.address);
 
 async function getBalances(d42) {
+	// Get the balance of the two accounts
 	const balance = await d42.balanceOf(owner.address);
 	const balanceOther = await d42.balanceOf(RECEIVER_ADDRESS);
 	// Get the number of decimals from the token contract
 	const decimals = await d42.decimals();
+	// Convert the wei values to tokens and print
+	console.log("=======================");
 	console.log("Owner balance:", ethers.utils.formatUnits(balance, decimals));
 	console.log("Other balance:", ethers.utils.formatUnits(balanceOther, decimals));
+	console.log("=======================");
 }
 
 async function main() {
+	// Get the contract factory object of our smart contract
 	const D42 = await ethers.getContractFactory("d42", owner);
+	// Deploy the contract on the testnet
 	const d42 = await D42.deploy(INITIAL_SUPPLY);
+	// Make sure to wait until it is deployed
 	await d42.deployed();
 
-	console.log("Contract deployed to:", d42.address);
+	const name = await d42.name();
+	const symbol = await d42.symbol();
+	console.log(`Name: ${name} | Symbol: ${symbol}`);
+
+	console.log(`\n✅ Contract deployed to address:", ${d42.address}\n`);
 
 	try {
 		// Connection test with Other (not supposed to succeed)
@@ -33,14 +48,15 @@ async function main() {
 		await getBalances(d42);
 
 		// Transfer tokens to 'Other'
-		const amount = ethers.utils.parseEther("10"); // sending 10 tokens
+		const amount = "10"; // sending 10 tokens
 
-		console.log(`Sending ${amount} tokens to ${RECEIVER_ADDRESS}...`);
+		console.log(`Sending ${amount} tokens to address: ${RECEIVER_ADDRESS}...`);
 
-		const tx = await d42.transfer(RECEIVER_ADDRESS, amount);
+		// Convert the transfered amount in wei
+		const tx = await d42.transfer(RECEIVER_ADDRESS, ethers.utils.parseEther(amount));
 		await tx.wait();
 
-  		console.log("✅ Transfer successful!");
+  		console.log("\n✅ Transfer successful!\n");
 
 		// Check updated balance of owner
 		await getBalances(d42);
